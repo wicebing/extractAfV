02b65844d34a87c6aaed64fb2beb2255add38160
wicebing <wicebing@gmail.com>
2023-07-08 23:29:42 +0800

    0708

--- v2a_s2.py
+++ v2a_s2.py
@@ -4,6 +4,13 @@ import sys
 import time
 import concurrent.futures
 import tqdm
+import logging
+import csv
+
+def setup_logging():
+    log_dir = 'log'
+    os.makedirs(log_dir, exist_ok=True)
+    logging.basicConfig(filename=os.path.join(log_dir, 'log.txt'), level=logging.ERROR, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
 def get_video_files(video_folder_path):
     video_files = [os.path.join(video_folder_path, v) for v in os.listdir(video_folder_path)]
@@ -12,21 +19,24 @@ def get_video_files(video_folder_path):
 def convert_to_audio(video_file, target_audio_folder_path):
     audio_file_name = os.path.splitext(os.path.basename(video_file))[0] + '.mp3'
     audio_file_path = os.path.join(target_audio_folder_path, audio_file_name)
-
-    with mp.VideoFileClip(video_file) as clip:
-        clip.audio.write_audiofile(audio_file_path)
-
-    print(f'=== {video_file} is done ===')
+    try:
+        with mp.VideoFileClip(video_file) as clip:
+            clip.audio.write_audiofile(audio_file_path)
+        print(f'=== {video_file} is done ===')
+    except Exception as e:
+        logging.error(f'Failed to convert {os.path.basename(video_file)} due to {e}')
+        return os.path.basename(video_file)
 
 def main(video_folder_path, max_workers):
+    setup_logging()
     video_files = get_video_files(video_folder_path)
 
     need_convert = len(video_files)
 
     if need_convert > 0:
         print(f'=== There are {need_convert} video files ===')
-        for video_file in video_files:
-            print('\t', video_file)
+        # for video_file in video_files:
+        #     print('\t', video_file)
         print('=============================')
     else:
         print('=== There are no video files ===')
@@ -36,11 +46,17 @@ def main(video_folder_path, max_workers):
     os.makedirs(target_audio_folder_path, exist_ok=True)
 
     start_time = time.time()
+    failed_files = []
 
     print(f'=== Using {max_workers} workers ===')
     with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
-        for _ in tqdm.tqdm(executor.map(convert_to_audio, video_files, [target_audio_folder_path]*len(video_files)), total=len(video_files)):
-            pass
+        results = list(tqdm.tqdm(executor.map(convert_to_audio, video_files, [target_audio_folder_path]*len(video_files)), total=len(video_files)))
+        failed_files = [r for r in results if r is not None]
+
+    with open('failed_files.csv', 'w', newline='') as f:
+        writer = csv.writer(f)
+        for file in failed_files:
+            writer.writerow([file])
 
     end_time = time.time()
     execution_time = end_time - start_time
